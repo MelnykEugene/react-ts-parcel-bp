ea9005f4ac2d96f07e2f30dec46a201febca9591
Eugene <melnyk.eugene.o@gmail.com>
2021-04-25 20:49:27 -0400

    first

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Greeting} from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+     <Greeting />
+    </React.StrictMode>,
+    document.getElementById("root")
+   );
